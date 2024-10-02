276ee62d32027dd30b5f56f775d132319b224168
aprevrha <aprevrha@gmail.com>
2024-10-02 20:46:59 +0200

    feat: Added player movement

--- src/matrix_affine.c
+++ src/matrix_affine.c
@@ -6,117 +6,21 @@
 /*   By: aprevrha <aprevrha@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:24:32 by aprevrha          #+#    #+#             */
-/*   Updated: 2024/10/02 13:00:26 by aprevrha         ###   ########.fr       */
+/*   Updated: 2024/10/02 17:42:12 by aprevrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-t_dvec2	rotation(double angle)
-{
-	double	cos_theta;
-	double	sin_theta;
-	t_dvec2	result;
+// t_dvec2	rotate(double angle)
+// {
+// 	double	cos_theta;
+// 	double	sin_theta;
+// 	t_dvec2	result;
 
-	cos_theta = cos(angle);
-	sin_theta = sin(angle);
-	ft_bzero(&result, sizeof(result));
-	result.x = cos_theta;
-	result.y = sin_theta;
-	return (result);
-}
-
-t_dvec2	rotate(double angle)
-{
-	double	cos_theta;
-	double	sin_theta;
-	t_dvec2	result;
-
-	cos_theta = cos(angle);
-	sin_theta = sin(angle);
-	ft_bzero(&result, sizeof(result));
-	result.x = cos_theta;
-	result.y = sin_theta;
-	return (result);
-}
-
-t_tmatrix	rotation_m_x(float angle)
-{
-	float		cos_theta;
-	float		sin_theta;
-	t_tmatrix	result;
-
-	cos_theta = cos(angle);
-	sin_theta = sin(angle);
-	ft_bzero(&result, sizeof(result));
-	result.f.x1 = 1;
-	result.f.y2 = cos_theta;
-	result.f.z2 = -sin_theta;
-	result.f.y3 = sin_theta;
-	result.f.z3 = cos_theta;
-	result.f.w4 = 1;
-	return (result);
-}
-
-t_tmatrix	rotation_m_y(float angle)
-{
-	float		cos_theta;
-	float		sin_theta;
-	t_tmatrix	result;
-
-	cos_theta = cos(angle);
-	sin_theta = sin(angle);
-	ft_bzero(&result, sizeof(result));
-	result.f.x1 = cos_theta;
-	result.f.z1 = sin_theta;
-	result.f.y2 = 1;
-	result.f.x3 = -sin_theta;
-	result.f.z3 = cos_theta;
-	result.f.w4 = 1;
-	return (result);
-}
-
-t_tmatrix	rotation_m_z(float angle)
-{
-	float		cos_theta;
-	float		sin_theta;
-	t_tmatrix	result;
-
-	cos_theta = cos(angle);
-	sin_theta = sin(angle);
-	ft_bzero(&result, sizeof(result));
-	result.f.x1 = cos_theta;
-	result.f.y1 = -sin_theta;
-	result.f.x2 = sin_theta;
-	result.f.y2 = cos_theta;
-	result.f.z3 = 1;
-	result.f.w4 = 1;
-	return (result);
-}
-
-t_tmatrix	scale_m(t_vec4 vec)
-{
-	t_tmatrix	result;
-
-	ft_bzero(&result, sizeof(result));
-	result.f.x1 = vec.f.x;
-	result.f.y2 = vec.f.y;
-	result.f.z3 = vec.f.z;
-	result.f.w4 = 1;
-	return (result);
-}
-
-t_tmatrix	translate_m(t_vec4 vec)
-{
-	t_tmatrix	result;
-
-	ft_bzero(&result, sizeof(result));
-	result.f.x1 = 1;
-	result.f.y2 = 1;
-	result.f.z3 = 1;
-	result.f.w4 = 1;
-	result.f.w1 = vec.f.x;
-	result.f.w2 = vec.f.y;
-	result.f.w3 = vec.f.z;
-	return (result);
-}
+// 	cos_theta = cos(angle);
+// 	sin_theta = sin(angle);
+// 	result.x = cos_theta;
+// 	result.y = sin_theta;
+// 	return (result);
+// }
